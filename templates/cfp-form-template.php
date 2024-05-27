f90fce7f46ce45df5f2d5e2850e67fd3b6a40e79
Sujit Shrestha <sujitshrestha0004@gmal.com>
2024-05-27 12:28:52 +0545

    added tailwind reference

--- templates/cfp-form-template.php
+++ templates/cfp-form-template.php
@@ -15,6 +15,8 @@ defined('ABSPATH') || exit;
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CFP Form</title>
+
+  <!-- Usingg tailwind css for css  -->
   <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
